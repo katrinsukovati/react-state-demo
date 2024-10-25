f96e25e8c1e0a6af92ec83876fc4cd0d313208a6
katrinsukovati <katrin.sukovati1@gmail.com>
2024-10-25 10:49:51 -0400

    basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,27 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import plants from "./data/plants.json";
 
 function App() {
+  // we want to store the entire object in initial state
+  const [selected, setSelected] = useState(plants[0]);
   return (
     <>
-      <h1>Heyyyy</h1>
+      <ul>
+        {plants.map((plant) => (
+          <li
+            key={plant.id}
+            onClick={() => {
+              setSelected(plant);
+              console.log(selected);
+            }}
+          >
+            {plant.name}
+          </li>
+        ))}
+      </ul>
+      <h1>{selected.name}</h1>
     </>
   );
 }
